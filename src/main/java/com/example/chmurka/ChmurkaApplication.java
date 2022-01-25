bb421782994c5e2c5958ebd49da8f02ff6fb5674
pizglut <szara.97@o2.pl>
2022-01-25 23:05:41 +0100

    Initial commit

---
+++ src/main/java/com/example/chmurka/ChmurkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.chmurka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChmurkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChmurkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chmurka/ChmurkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chmurka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChmurkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
